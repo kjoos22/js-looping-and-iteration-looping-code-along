659b25a37d9f7d77cd52363813abccbbc84c08ed
kjoos22 <kjoos22@gmail.com>
2021-03-15 17:55:23 -0400

    commit tree 23d4d6a8d60a59b0b43532171cfec5607bfececa

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+// Code your solutions in this file
+function writeCards(names, event) {
+    let cards = [];
+    for(let x = 0; x <= names.length - 1; x++) {
+        cards.push(`Thank you, ${names[x]}, for the wonderful ${event} gift!`);
+    }
+    return cards;
+}
+
+function countDown(start) {
+    while (start >= 0) {
+        console.log(start);
+        start--;
+    }
+}
